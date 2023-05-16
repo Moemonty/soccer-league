d13160817ba3d93a6906442d1b6b5ef279a9c09b
Moe Monty <moe@moe.com>
2023-05-16 16:49:41 -0500

    Updated, but need to work out bugs

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ def parse
   if ARGV[0]
     filename = ARGV[0]
     read_from_argument(filename)
-
-    puts result
   else
     if $stdin
       read_from_stdin
@@ -19,36 +17,37 @@ end
 
 def read_from_argument(filename)
   team_obj = {}
+  match_day_count = 0
 
   File.open(filename, 'r') do |file|
     file.each_line do |match|
       # For each line, split by comman
       match_details = match.split(',')
-      puts match_details
+
       team_1 = match_details[0]
       team_2 = match_details[1]
 
       team_1_name = team_1.gsub(/\d/, '').strip
       team_1_score = team_1[/\d+/]
 
-      puts team_1_name
-      puts team_1_score
-
       team_2_name = team_2.gsub(/\d/, '').strip
       team_2_score = team_2[/\d+/]
 
 
-      # Calculate which team one, tied or lost
-      result = determine_winner_and_score(team_1_score, team_2_score)
-
-      puts result
-
       # IF ONE TEAM ALREADY EXISTS, THEY ALL EXIST, so report team object and clear out that day's info
-      if team_obj[team_1_name]
-        puts "The Matchday is over, not start iterating through the object and return the top team"
+      if team_obj.has_key?(team_1_name)
+        match_day_count += 1
+        puts "Matchday #{match_day_count}"
+        results = parse_and_print_matchday(team_obj)
+        results.each do |result|
+          puts result
+        end
 
-        puts team_obj
+        #clear out team_obj
+        # team_obj = {}
       end
+
+      result = determine_winner_and_score(team_1_score, team_2_score)
 
       # initialize if don't exist || IF THEY DO Exist, its another match day
       if !team_obj[team_1_name] && !team_obj[team_2_name]
@@ -65,30 +64,8 @@ def read_from_argument(filename)
           team_obj[team_2_name] = result[:score]
         end
       end
-      # if !team_obj[team_1_name]
-      #   if result[:winner] == 1
-      #     team_obj[team_1_name] = result[:score]
-      #     team_obj[team_2_name] = 0
-      #   end
-      #
-      #   if result[:winner] == "tied"
-      #     team_obj[team_1_name] = result[:score]
-      #   end
-      # end
-      #
-      # if !team_obj[team_2_name]
-      #   if result[:winner] == 2
-      #     team_obj[team_2_name] = result[:score]
-      #   end
-      #
-      #   if result[:winner] == "tied"
-      #     team_obj[team_2_name] = result[:score]
-      #   end
-      # end
 
 
-      puts team_obj
-      puts team_obj
     end
   end
 end
@@ -111,6 +88,23 @@ def determine_winner_and_score(team1_score, team2_score)
   if team1_score == team2_score
     return { winner: 'tied', score: 1}
   end
+end
+
+def parse_and_print_matchday(team_obj)
+  sorted_hash = team_obj.sort do |(key1, value1), (key2, value2)|
+    [value2, key1.downcase] <=> [value1, key2.downcase]
+  end
+  # https://ruby-doc.org/core-2.2.2/Hash.html -- Hashes retain order
+
+  # Take the first three items
+  top_teams = sorted_hash.to_a.take(3).to_h
+
+  result = []
+  top_teams.each do |key, val|
+    result << "#{key}, #{val} pts"
+  end
+
+  result
 end
 
 def read_from_file
