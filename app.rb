3c9ee6c06ad4cc1cd1baf9ad9967965dc3fae52e
Moe Monty <moe@moe.com>
2023-05-16 19:42:02 -0500

    Refactored input

--- app.rb
+++ app.rb
@@ -2,7 +2,17 @@ def main
   # Get Filename if passed as argument
   if ARGV[0]
     filename = ARGV[0]
-    read_from_argument(filename)
+    array_of_lines = []
+    line_count = 0
+
+    File.open(filename, 'r') do |file|
+      file.each_line do |line|
+        array_of_lines << line
+        line_count += 1
+      end
+    end
+
+    read_lines_global(array_of_lines, line_count)
   else
     array_of_lines = []
     line_count = 0
@@ -13,21 +23,16 @@ def main
         line_count += 1
       end
 
-      read_from_stdin(array_of_lines, line_count)
+      read_lines_global(array_of_lines, line_count)
     end
   end
 end
 
-def read_from_argument(filename)
+def read_lines_global(matches, line_count)
   team_obj = {}
-  line_count = 0
+  line_count = line_count
   match_day = 0
 
-  File.open(filename, 'r') do |file|
-    file.each_line do |line|
-      line_count += 1
-    end
-  end
 
   curr_line = 1
   # We have line count
@@ -36,56 +41,6 @@ def read_from_argument(filename)
   # and we know there are always two teams playing, we divide by that
   # so Lines of input / Half, one outcome per line of input / and divided again by 2, because two teams
   # so 24 lines of input would results in (12 / 2 / 2) == 3
-  match_day_end = (line_count / 2) / 2
-
-  File.open(filename, 'r') do |file|
-    file.each_line do |match|
-      match_info = parse_match_info(match)
-      result = determine_winner_and_score(match_info[:team_1_score], match_info[:team_2_score])
-
-      if !team_obj.has_key?(match_info[:team_1_name])
-        team_obj[match_info[:team_1_name]] = 0
-      end
-
-      if !team_obj.has_key?(match_info[:team_2_name])
-        team_obj[match_info[:team_2_name]] = 0
-      end
-
-      if result[:winner] == 1
-        team_obj[match_info[:team_1_name]] += result[:score]
-        team_obj[match_info[:team_2_name]] += 0
-      end
-      if result[:winner] == 2
-        team_obj[match_info[:team_2_name]] += result[:score]
-        team_obj[match_info[:team_1_name]] += 0
-      end
-
-      if result[:winner] == 0
-        team_obj[match_info[:team_1_name]] += result[:score]
-        team_obj[match_info[:team_2_name]] += result[:score]
-      end
-
-      # Determined when to print from input (current line / matches / teams per match) 12 / 2 / 2 == 3
-      if curr_line % match_day_end == 0
-        match_day += 1
-        puts "Match Day #{match_day}"
-        parse_and_print_matchday(team_obj)
-        puts "\n"
-      end
-
-      curr_line += 1
-    end
-  end
-end
-
-def read_from_stdin(matches, line_count)
-  team_obj = {}
-  match_day = 0
-  line_count = line_count
-
-  puts line_count
-
-  curr_line = 1
   match_day_end = (line_count / 2) / 2
 
   matches.each do |match|
@@ -108,6 +63,7 @@ def read_from_stdin(matches, line_count)
       team_obj[match_info[:team_2_name]] += result[:score]
       team_obj[match_info[:team_1_name]] += 0
     end
+
     if result[:winner] == 0
       team_obj[match_info[:team_1_name]] += result[:score]
       team_obj[match_info[:team_2_name]] += result[:score]
